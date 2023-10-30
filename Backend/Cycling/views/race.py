fffb71485f93ea017764cc97714e65dcf0be4942
Wannes-VanPuyvelde <r0704704@ucll.be>
2023-10-30 16:58:36 +0100

    Added calculate_score functions

--- Backend/Cycling/views/race.py
+++ Backend/Cycling/views/race.py
@@ -8,6 +8,7 @@ from rest_framework.response import Response
 from django.contrib.auth import  get_user_model
 from procyclingstats import Race, RaceStartlist, Stage
 from Cycling.models import Renner, Tour, Team
+from django.http import QueryDict
 
 
 giro_latest = Race('/'.join(max(Race('/race/giro-d-italia/2020').prev_editions_select(), key=lambda entry: int(entry["text"]))['value'].split('/')[0:3]))
@@ -102,21 +103,35 @@ def get_stage_info(request):
 		return Response({'error': str(e)}, status=400)
 
 @api_view(['GET'])
-def calculate_score(request):
+def calculate_score_per_renner_per_stage(request):
     stage_name = request.GET.get('stage_name')
     if not stage_name:
         return Response({'error': 'invalid stage data'}, status=400)
 
     stage = Stage(stage_name)
-    gc = stage.gc()[0]['rider_name']
-    kom = stage.kom()[0]['rider_name']
-    point = stage.points()[0]['rider_name']
+    
+    try: 
+        gc = stage.gc()[0]['rider_name']
+        
+    except IndexError:
+        gc = ""
+        
+    try: 
+        kom = stage.kom()[0]['rider_name']
+        
+    except IndexError:
+        kom = ""    
+        
+    try: 
+        point = stage.points()[0]['rider_name']
+        
+    except IndexError:
+        point = ""    
     
     try:
-        youth_data = stage.youth()[0]
-        youth_name = youth_data['rider_name']
+        youth = stage.youth()[0]['rider_name']
     except IndexError:
-        youth_name = ""
+        youth = ""
 
     point_array = [100, 80, 65, 55, 45, 35, 30, 25, 20, 17, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 
@@ -129,25 +144,71 @@ def calculate_score(request):
 
     for rider in stage.results():
         points = calculate_points(rider['rank'])
+        total_points = points
         shirt_points = 0
+        shirts = []
 
         if rider['rider_name'] == gc:
             shirt_points += 20
-            points += 20
+            total_points += 20
+            shirts.append("gc")
 
         if rider['rider_name'] == kom:
             shirt_points += 10
-            points += 10
+            total_points += 10
+            shirts.append("kom")
 
         if rider['rider_name'] == point:
             shirt_points += 10
-            points += 10
+            total_points += 10
+            shirts.append("point")
 
-        if rider['rider_name'] == youth_name:
+        if rider['rider_name'] == youth:
             shirt_points += 5
-            points += 5
+            total_points += 5
+            shirts.append("youth")
 
-        total.append({'rider_name': rider['rider_name'], 'rider_rank': rider['rank'], 'points': points, 'shirt points': shirt_points})
+        total.append({'rider_name': rider['rider_name'], 'rider_rank': rider['rank'],'shirts': shirts, 'points': points, 'shirt_points': shirt_points, 'total_points': total_points})
 
     return Response({"test": total}, status=200)
 
+@api_view(['GET'])
+def calculate_score_per_renner_per_tour(request):
+    tour_name = request.GET.get('tour_name')
+    if not tour_name:
+        return Response({'error': 'invalid tour data'}, status=400)
+    stages = Race(tour_name).stages()
+    stage_urls = [item['stage_url'] for item in stages]
+    rider_summary = {}
+    
+    for url in stage_urls:
+		# Create a new QueryDict for each stage URL
+        query_dict = QueryDict(mutable=True)
+        query_dict['stage_name'] = url
+        drf_request = request._request
+        drf_request.GET = query_dict
+        
+        # Call your DRF view or function with the Django request
+        response = calculate_score_per_renner_per_stage(drf_request)
+        
+        if response.status_code == 200:
+            stage_results = response.data.get('test', [])
+
+            # Aggregate results for each rider
+            for result in stage_results:
+                rider_name = result['rider_name']
+                if rider_name not in rider_summary:
+                    rider_summary[rider_name] = {
+                        'points': 0,
+                        'shirt_points': 0,
+                        'total_points': 0,
+                    }
+
+                rider_summary[rider_name]['points'] += result['points']
+                rider_summary[rider_name]['shirt_points'] += result['shirt_points']
+                rider_summary[rider_name]['total_points'] += result['total_points']
+    
+	# Convert the rider_summary dictionary to a list
+    rider_summary_list = [{'rider_name': name, **data} for name, data in rider_summary.items()]
+
+    return Response({"rider_summary": rider_summary_list}, status=200)
