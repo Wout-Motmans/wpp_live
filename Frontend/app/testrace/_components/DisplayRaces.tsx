241ebf8cd6d2e427f27fd2cf548e2993b11b7485
Saba_Sulakadze <saba.sulakadze@student.ucll.be>
2023-11-10 19:24:32 +0100

    changes

--- Frontend/app/testrace/_components/DisplayRaces.tsx
+++ Frontend/app/testrace/_components/DisplayRaces.tsx
@@ -1,7 +1,9 @@
 
 import {  useEffect, useState } from 'react';
-import { Table } from 'antd';
+import { Button, Table, Input } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
+import { SearchProps } from 'antd/es/input/Search';
+const { Search } = Input;
 
 
 interface RaceInfo {
@@ -31,23 +33,57 @@ export default function DisplayRaces({ setChosenRace } : { setChosenRace : (valu
         },
     ];
 
+    function handleCreateCustomTour() {
+
+    }
+
+    
+    const onSearch: SearchProps['onSearch'] = (value, _e, info) => {
+
+    }
 
     return (
-        <Table
-        columns={columns}
-        bordered
-        dataSource={races}
-        pagination={false}
-        rowSelection={{
-            type:'radio',
-            onSelect:(record) => setChosenRace(record.key)
-        }}
-        />
+        <div className='flex flex-col space-y-2'>
+            <Button onClick={handleCreateCustomTour} >Create Custom Tour</Button>
+            <Search placeholder='add race' onSearch={onSearch} enterButton/>
+            <Table
+                
+                columns={columns}
+                bordered
+                dataSource={races}
+                pagination={false}
+                rowSelection={{
+                    type:'radio',
+                    onSelect:(record) => setChosenRace(record.key)
+                }}
+            />
+        </div>
     )
 }
 
 
 const getNewestRaces = async (): Promise<RaceInfo[]> => {
+    try {
+        const response = await fetch('/api/popraces');
+    
+        if (!response.ok) throw new Error('popraces error');
+        
+        const data = await response.json();
+        const modifiedData: RaceInfo[] = data.map((race: { url: string; name: string; year: string; }) => ({
+            key: race.url,
+            name: race.name,
+            year: race.year,
+        }));
+        
+        return modifiedData;
+
+    } catch (error) {
+        console.error('Popraces error:', error);
+        throw error;
+    }
+};
+
+const findRace = async (race : string): Promise<RaceInfo[]> => {
     try {
         const response = await fetch('/api/popraces');
     
