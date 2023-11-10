576151760aece992cde4e79924f4e474c1685981
Nikolai Daelemans <nikolaidaelemans@Nikolais-MacBook-Pro.local>
2023-11-10 16:25:15 +0100

    404 error page

--- Frontend/app/not-found.js
+++ Frontend/app/not-found.js
@@ -4,28 +4,52 @@ import Link from 'next/link';
 const NotFound = () => {
     const url = document.URL
     return (
-        <div className='flex-center-container' style={{display:'flex',flexDirection:'column',gap:'20px',textAlign:'center', marginTop:'12em'}}>
-            <div style={{padding:'2em' ,border:'solid white 2px', backgroundColor:'grey', color:'black'}} className='content'>
+        <div className='flex-center-container' style={{display:'flex',flexDirection:'column',textAlign:'center', marginTop:'12em'}}>
+            <div style={{width: "100%", overflow: "hidden" }}>
+                <img
+                    src='/img/bicycleman3.gif'
+                    alt='Bicycle Animation'
+                    className='animated-image'
+                />
+
+                <style jsx>{`
+                    @keyframes slide {
+                        0% {
+                            transform: translateX(-10vw);
+                          }
+                          100% {
+                            transform: translateX(100vw);
+                          }
+                    }
+
+                    .animated-image {
+                    width: 120px;
+                    height: 90px;
+                    animation: slide 7s linear infinite; 
+                    }
+                `}</style>
+            </div>
+            <div style={{padding:'2em' ,border:'solid white 2px', backgroundColor:'#f55525', color:'black'}} className='content'>
                 <div style={{display:'flex', gap:'3px',justifyContent:'center' ,alignItems:'center'}}>
                 <h1 style={{fontSize:'6em'}}>4</h1>
                 <img src='/img/wiel.gif' style={{width:'75px',height:'75px',}}></img>
                 <h1 style={{fontSize:'6em'}}>4</h1>
                 </div>
-                <h2>{url} doesn't exist, please head back to the home page :)</h2>
+                <h2>{url} doesn't exist, please 'cycle' back to the home page.</h2>
             </div>
 
             <div className='content'>
                 <Link style={{
                     display: 'inline-block',
                     padding: '10px 20px',
-                    backgroundColor: 'grey', // Change the background color as needed
-                    color: 'black', // Change the text color as needed
+                    backgroundColor: 'black', // Change the background color as needed
+                    color: 'white', // Change the text color as needed
                     borderRadius: '5px',
                     textDecoration: 'none',
                     transition: 'background-color 0.3s ease'
                 }} href="/" id='button'
-                onMouseOver={(e) => e.currentTarget.style.backgroundColor = '#646566'}
-                onMouseOut={(e) => e.currentTarget.style.backgroundColor = 'grey'}>
+                onMouseOver={(e) => e.currentTarget.style.backgroundColor = 'grey'}
+                onMouseOut={(e) => e.currentTarget.style.backgroundColor = 'black'}>
                     Go back to Home
                 </Link>
             </div>
