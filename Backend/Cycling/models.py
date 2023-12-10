5e173a910a909e2d82d173187b726984d60558c9
Wannes-VanPuyvelde <r0704704@ucll.be>
2023-12-10 17:02:23 +0100

    Updated models.py again

--- Backend/Cycling/models.py
+++ Backend/Cycling/models.py
@@ -41,10 +41,11 @@ class GameTeam(models.Model):
 
 class Rider(models.Model):
     id = models.AutoField(primary_key=True)
-    full_name = models.CharField(max_length=255)
-    url = models.CharField(max_length=255)
-    real_team = models.CharField(max_length=255)
-    real_team_name = models.CharField(max_length=255)
+    rider_name = models.CharField(max_length=255)
+    rider_url = models.CharField(max_length=255)
+    team_url = models.CharField(max_length=255)
+    team_name = models.CharField(max_length=255)
+    nationality = models.CharField(max_length=255)
 
 class RiderGameTeam(models.Model):
     id = models.AutoField(primary_key=True)
